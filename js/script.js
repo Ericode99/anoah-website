a95bd8c8af5bfd49d307ca2fd6f7dc09eb5b7144
Ericode99 <eric.rudischhauser@gmail.com>
2022-03-26 16:09:43 +0100

    added sticky header and menu functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,65 @@
 "use strict";
+
+// MENU FUNCTIONALITY
+
+const btnNav = document.querySelector(".btn-nav");
+const headerEl = document.querySelector(".header");
+
+btnNav.addEventListener("click", function () {
+  console.log("click");
+  headerEl.classList.toggle("nav-open");
+});
+
+/////////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  const href = link.getAttribute("href");
+  if (href.startsWith("#")) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      // Scroll back to top
+      if (href === "#")
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+
+      // Scroll to other links
+      if (href !== "#" && href.startsWith("#")) {
+        const sectionEl = document.querySelector(href);
+        sectionEl.scrollIntoView({ behavior: "smooth" });
+      }
+    });
+  }
+});
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0.3,
+    rootMargin: "",
+  }
+);
+obs.observe(sectionHeroEl);
