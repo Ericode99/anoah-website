6c663271115ac470298737abc3b62932a55c3608
Ericode99 <eric.rudischhauser@gmail.com>
2022-03-31 20:27:50 +0200

    added menu page

---
+++ js/menu-script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// MENU FUNCTIONALITY
+
+const btnNav = document.querySelector(".btn-nav");
+const headerEl = document.querySelector(".header");
+
+btnNav.addEventListener("click", function () {
+  console.log("click");
+  headerEl.classList.toggle("nav-open");
+});
+
+/////////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  const href = link.getAttribute("href");
+  if (href.startsWith("#")) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      // Scroll back to top
+      if (href === "#")
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+
+      // Scroll to other links
+      if (href !== "#" && href.startsWith("#")) {
+        const sectionEl = document.querySelector(href);
+        sectionEl.scrollIntoView({ behavior: "smooth" });
+      }
+    });
+  }
+});
