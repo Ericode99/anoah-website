0c7ffa7e4ae9f1a7f85cc0e4a62531075f34930e
Ericode99 <eric.rudischhauser@gmail.com>
2022-04-04 18:55:56 +0200

    added footer on all pages

--- js/team-script.js
+++ js/team-script.js
@@ -1,5 +1,10 @@
 "use strict";
 
+//Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
 // MENU FUNCTIONALITY
 
 const btnNav = document.querySelector(".btn-nav");
